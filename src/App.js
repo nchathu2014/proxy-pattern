6ab1bc434ae6e985ad39144e1f12271ce04c6933
Nuwan Chathuranga <nchathu2014@gmail.com>
2021-12-31 11:06:23 +0530

    proxy pattern proxy pattern basic implementation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./styles.css";
+import proxyImage from "./img/dp.JPG";
+
+import personProxy from "./utils/proxy";
+import { useEffect } from "react";
 
 export default function App() {
+  useEffect(() => {
+    personProxy.name = "Nuwan";
+    personProxy.age = 55;
+    personProxy.nationality = "Canadian";
+  }, []);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Proxy Design Pattern - Demo</h1>
+      <h3>Use Case: A proxy can be useful to add validation. </h3>
+      <img src={proxyImage} alt="Proxy Image" />
+
+      <hr />
+      <h3>Please refer to the console</h3>
     </div>
   );
 }
---
+++ src/utils/proxy.js
@@ -0,0 +1,33 @@
+const person = {
+  name: "John Doe",
+  age: 42,
+  nationality: "American"
+};
+
+// Crate a proxy object
+
+const personProxy = new Proxy(person, {
+  get: (obj, key) => {
+    //check for existance of properties
+    if (!obj[key]) {
+      console.error(`${key} property doesn't exists`);
+    } else {
+      console.log(`The value of ${key} is ${obj[key]}`);
+    }
+  },
+  set: (obj, key, value) => {
+    if (key === "name" && typeof value !== "string" && value.length <= 2) {
+      console.log(`Sorry, provide a valid name`);
+    } else if (key === "age" && typeof value !== "number") {
+      console.log(`Sorry, age should be a number`);
+    } else if (key === "nationality" && typeof value !== "string") {
+      console.log(`Sorry, provide a valid nationality`);
+    } else {
+      console.log(`Changed ${key} from ${obj[key]} to ${value}`);
+      obj[key] = value;
+    }
+    return true;
+  }
+});
+
+export default personProxy;
